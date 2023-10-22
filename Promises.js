a0a9b34a3733d8f96437da3bb64044d51e3c71a8
Satyam Garg <113013930+garg2002@users.noreply.github.com>
2023-10-22 22:05:24 +0530

    Promises in js

---
+++ Promises.js
@@ -0,0 +1,15 @@
+
+let countValue = new Promise(function (resolve, reject) {
+    reject('Promise rejected'); 
+ });
+ 
+ countValue.then(
+     function successValue(result) {
+         console.log(result);
+     },
+  )
+ .catch(
+     function errorValue(result) {
+         console.log(result);
+     }
+ );
